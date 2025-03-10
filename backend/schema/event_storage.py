4051fc0ded9b7b4fc5dbe344109de84c4e12db0a
rexong <rexonghanwei200035@gmail.com>
2025-03-10 21:54:43 +0800

    update create and update event storage

--- backend/schema/event_storage.py
+++ backend/schema/event_storage.py
@@ -11,9 +11,14 @@ class EventStorage:
             timestamp,
             town, 
             street,
-            congestion_level,
-            speed,
-            end_node
+            x,
+            y,
+            alert_type,
+            alert_subtype,
+            reliability,
+            image_src="",
+            current_speed="",
+            previous_speed=""
         ):
         event_id = self.counter
         self.events[event_id] = event
@@ -23,9 +28,14 @@ class EventStorage:
             timestamp,
             town,
             street,
-            congestion_level,
-            speed,
-            end_node
+            x,
+            y,
+            alert_type,
+            alert_subtype,
+            reliability,
+            image_src,
+            current_speed,
+            previous_speed
         )
         return event_id
 
@@ -40,9 +50,14 @@ class EventStorage:
             timestamp=None,
             town=None, 
             street=None,
-            congestion_level=None,
-            speed=None,
-            end_node=None,
+            x=None,
+            y=None,
+            alert_type=None,
+            alert_subtype=None,
+            reliability=None,
+            image_src=None,
+            current_speed=None,
+            previous_speed=None,
             score=None
         ):
         event = self.events.get(event_id, None)
@@ -53,12 +68,22 @@ class EventStorage:
                 event.town = town
             if street:
                 event.street = street
-            if congestion_level:
-                event.congestion_level = congestion_level
-            if speed:
-                event.speed = speed
-            if end_node:
-                event.end_node = end_node
+            if x:
+                event.x = x
+            if y:
+                event.y = y
+            if alert_type:
+                event.alert_type = alert_type
+            if alert_subtype:
+                event.alert_subtype = alert_subtype
+            if reliability:
+                event.reliability = reliability
+            if image_src:
+                event.image_src = image_src
+            if current_speed:
+                event.current_speed = current_speed
+            if previous_speed:
+                event.previous_speed = previous_speed
             if score:
                 event.score = score
             return event
