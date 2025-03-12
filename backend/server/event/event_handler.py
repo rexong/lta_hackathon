f727d868f53eb7fcc20b420def7f896247872ef3
rexong <rexonghanwei200035@gmail.com>
2025-03-13 01:10:03 +0800

    feat: add logging

--- backend/server/event/event_handler.py
+++ backend/server/event/event_handler.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 from flask import request, jsonify
 from backend.schema.event import Event
 from backend.server.event.manager.crowdsource_manager import CrowdsourceManager
@@ -21,6 +25,8 @@ def get_manager(storage_type):
     return MANAGERS[storage_type]
 
 def create_event_from_crowdsource():
+    logger.info("Flask: POST /event/crowdsource Invoked")
+
     manager: CrowdsourceManager = MANAGERS['crowdsource']
     data = request.get_json()
 
@@ -28,6 +34,7 @@ def create_event_from_crowdsource():
     return jsonify(event.to_dict()), 201
 
 def add_verified_event():
+    logger.info("Flask: POST /events/verified Invoked")
     manager: VerifiedManager = MANAGERS['verified']
     data = request.get_json()
     verified_event = Event.from_dict(data)
@@ -36,6 +43,7 @@ def add_verified_event():
     return event
 
 def get_all_events(storage_type):
+    logger.info(f"Flask: GET /events/{storage_type} Invoked")
     manager = get_manager(storage_type)
     if not manager:
         return jsonify({'message': f'Event Storage {storage_type} not found!'}), 400
@@ -43,6 +51,7 @@ def get_all_events(storage_type):
     return jsonify([event.to_dict() for event in events]), 200
     
 def get_one_event(storage_type, event_id):
+    logger.info(f"Flask: GET /events/{storage_type}/{event_id} Invoked")
     manager = get_manager(storage_type)
     if not manager:
         return jsonify({'message': f'Event Storage {storage_type} not found!'}), 400
