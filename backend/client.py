9a6f57a5319f8f33e7e98547f5c65357993a92ab
rexong <rexonghanwei200035@gmail.com>
2025-03-16 14:07:42 +0800

    fix: change to localhost

--- backend/client.py
+++ backend/client.py
@@ -1,7 +1,7 @@
 import requests
 from sseclient import SSEClient
 
-URL = "http://127.0.0.1:5000"
+URL = "http://localhost:5000"
 CROWDSOURCE_URI = "store/crowdsource"
 FILTERED_URI = "store/filtered"
 
