3a6a6c7dec2fd15d7bdd33c277b6d67b87555970
rexong <rexonghanwei200035@gmail.com>
2025-03-16 15:27:01 +0800

    feat: add filtered event sse

--- backend/client.py
+++ backend/client.py
@@ -5,11 +5,25 @@ URL = "http://localhost:5000"
 CROWDSOURCE_URI = "store/crowdsource"
 FILTERED_URI = "store/filtered"
 
-def listen_to_sse():
-    response = requests.get(f'{URL}/{CROWDSOURCE_URI}', stream=True)
-    client = SSEClient(response)
-    for event in client.events():
-        print(f"Received event id: {event.data}")
+def listen_to_sse(uri):
+    try:
+        response = requests.get(f'{URL}/{uri}', stream=True)
+        response.raise_for_status()  
+        client = SSEClient(response)
+        for event in client.events():
+            print(f"Received event id: {event.data}")
+    except requests.exceptions.ConnectionError:
+        print("Error: Unable to connect to the backend. Please make sure the server is running.")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def listen_to_crowdsource():
+    listen_to_sse(CROWDSOURCE_URI)
+
+def listen_to_filtered():
+    listen_to_sse(FILTERED_URI)
+    
 
 if __name__ == "__main__":
     listen_to_sse()
